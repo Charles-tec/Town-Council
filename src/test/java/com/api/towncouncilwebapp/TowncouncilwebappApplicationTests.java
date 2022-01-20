ec64fefe3fc8bae98723f57bc2f986a058d22094
Charles-tec <charlesnyashaphiri02@gmail.com>
2022-01-20 12:06:36 +0200

    LoginIn and authorisation API

---
+++ src/test/java/com/api/towncouncilwebapp/TowncouncilwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.towncouncilwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TowncouncilwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
